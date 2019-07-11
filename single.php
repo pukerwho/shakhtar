bb165a4957ac5be47c85d0398877785a6580f246
Vladimir Samojlov <v-moi@ya.ru>
2019-07-12 01:06:14 +0300

    edit single

--- single.php
+++ single.php
@@ -1,12 +1,25 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="single__hero" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>')">
+	<div class="single__hero pb-5" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>');background-size: cover; -webkit-background-size: cover; background-position: top;">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
 					<div class="single__hero-title">
 						<?php the_title(); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="single-post">
+		<div class="container">
+			<div class="row py-5">
+				<div class="col-md-12">
+					<div class="single-post__content">
+						<div class="single-post__content-inner">
+							<?php the_content(); ?>		
+						</div>
 					</div>
 				</div>
 			</div>
